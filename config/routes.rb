ea2180648f6490d20f4b136af9061ef7fc67a8f0
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-10-08 16:33:00 +0000

    exclude photo index route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :follow_requests, except: [:index, :edit, :show, :new]
   resources :likes, only: [:create, :destroy]
-  resources :photos
+  resources :photos, except: [:index]
 
   get ":username" => "users#show", as: :user
   get ":username/liked" => "users#liked", as: :liked
