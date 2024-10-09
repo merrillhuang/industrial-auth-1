0712a97527e55d760c4748f14a20f233385471c9
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-10-09 16:21:31 +0000

    add authorization to user feed action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
 
+  def feed
+    authorize @user
+  end
+
   private
 
     def set_user
