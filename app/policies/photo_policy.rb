1a9a0b8f8534447e370a2964d36c1feb96549ea8
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-10-09 14:41:08 +0000

    create photo show policy

--- app/policies/photo_policy.rb
+++ app/policies/photo_policy.rb
@@ -1,2 +1,16 @@
 class PhotoPolicy
+  attr_reader :user, :photo
+
+  def initialize(user, photo)
+    @user = user
+    @photo = photo
+  end
+
+  # Our policy is that a photo should only be seen by the owner or followers
+  #   of the owner, unless the owner is not private in which case anyone can
+  #   see it
+
+  def show?
+    user == photo.owner || photo.owner.followers.includes?(user) || !photo.owner.private?
+  end
 end
