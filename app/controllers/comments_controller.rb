2a1fd0bece7a503be1c1a7533af700e5757ea019
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-11-04 17:57:43 +0000

    remove unnecessary is_author, using pundit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
-  before_action :is_author, only: [:destroy, :edit, :update]
   before_action :is_an_authorized_user, only: [:create]
 
   # GET /comments or /comments.json
